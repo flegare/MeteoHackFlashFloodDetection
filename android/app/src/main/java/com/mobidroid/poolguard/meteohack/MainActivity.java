2b83087d8b1ceb894156307914fe7f7db551a3ca
Francois Legare <francois.legare@bell.ca>
2019-07-10 16:46:00 -0400

    added the android app stub

---
+++ android/app/src/main/java/com/mobidroid/poolguard/meteohack/MainActivity.java
@@ -0,0 +1,183 @@
+package com.mobidroid.poolguard.meteohack;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity";
+    private TextView editLocation;
+    private LocationManager locationManager;
+    private MyLocationListener locationListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+
+        //Set location listener on.
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        editLocation = (TextView)findViewById(R.id.editLocation);
+        locationListener = new MyLocationListener();
+
+        /*
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        */
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button() {
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this,
+                ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                request_permission();
+            }
+        } else {
+            // permission has been granted
+            locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void request_permission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                ACCESS_COARSE_LOCATION)) {
+
+            Snackbar.make(findViewById(R.id.mainLayout), "Location permission is needed because ...",
+                    Snackbar.LENGTH_LONG)
+                    .setAction("retry", new View.OnClickListener() {
+                        @RequiresApi(api = Build.VERSION_CODES.M)
+                        @Override
+                        public void onClick(View view) {
+                            requestPermissions(new String[]{ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 10);
+                        }
+                    })
+                    .show();
+        } else {
+            // permission has not been granted yet. Request it directly.
+            requestPermissions(new String[]{ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 10);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /*---------- Listener class to get coordinates ------------- */
+    private class MyLocationListener implements LocationListener {
+
+
+
+        @Override
+        public void onLocationChanged(Location loc) {
+
+            String longitude = "Longitude: " + loc.getLongitude();
+            Log.e(TAG, longitude);
+            String latitude = "Latitude: " + loc.getLatitude();
+            Log.e(TAG, latitude);
+
+            editLocation.setText("Longitude: " + loc.getLongitude() + " Latitude: " + loc.getLatitude() );
+
+
+            /*------- To get city name from coordinates --------
+            String cityName = null;
+            Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+            List<Address> addresses;
+            try {
+                addresses = gcd.getFromLocation(loc.getLatitude(),
+                        loc.getLongitude(), 1);
+                if (addresses.size() > 0) {
+                    System.out.println(addresses.get(0).getLocality());
+                    cityName = addresses.get(0).getLocality();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            String s = longitude + "\n" + latitude + "\n\nMy Current City is: "
+                    + cityName;
+            editLocation.setText(s);
+
+            */
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+    }
+
+}
